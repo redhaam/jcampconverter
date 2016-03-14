29b72c0529840d83db7c4f4bc6f42fbb4baf0bfc
Michaël Zasso <mic.besace@gmail.com>
2016-03-14 15:04:54 +0100

    new format for GC/MS data

--- test/testGCMS.js
+++ test/testGCMS.js
@@ -3,28 +3,39 @@
 var Converter = require('..');
 var fs = require('fs');
 
-
-
 describe('Test JCAMP converter of GCMS', function () {
+    var file = fs.readFileSync(__dirname + '/data/misc/gcms.jdx').toString();
 
-    var result = Converter.convert(fs.readFileSync(__dirname + '/data/misc/gcms.jdx').toString());
-    var gcms=result.gcms;
+    describe('old format', function () {
+        var result = Converter.convert(file);
+        var gcms=result.gcms;
 
-    it('Check content', function () {
-        gcms.should.keys(['gc','ms']);
-        gcms.gc.should.be.type('object');
-        gcms.ms.should.be.type('object');
-        gcms.gc.tic.should.be.instanceof(Array);
+        it('Check content', function () {
+            gcms.should.keys(['gc','ms']);
+            gcms.gc.should.be.type('object');
+            gcms.ms.should.be.type('object');
+            gcms.gc.tic.should.be.instanceof(Array);
+        });
+
+        it('Check length', function () {
+            gcms.gc.tic.length.should.be.equal(4840);
+            gcms.ms.length.should.be.equal(2420);
+        });
     });
 
-    it('Check length', function () {
-        gcms.gc.tic.length.should.be.equal(4840);
-        gcms.ms.length.should.be.equal(2420);
+    describe('new format', function () {
+        var result = Converter.convert(file, {newGCMS: true});
+        var gcms=result.gcms;
+
+        it('Check content', function () {
+            gcms.should.keys(['times','series']);
+            gcms.times.should.be.Array();
+            gcms.series.should.be.Array().with.lengthOf(3); // ms, tic, scannumber
+        });
+
+        it('Check length', function () {
+            gcms.times.length.should.equal(2420);
+            gcms.series[0].data.length.should.equal(2420);
+        });
     });
-
-
-    //console.log(Object.keys(gcms));
-
-    //console.log(gcms.tic);
-
 });
