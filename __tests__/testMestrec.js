5fec72dc92d2cd2baca19eb402dbbcd3f8f6605c
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-09-12 23:06:24 -0500

    test(mestrec): add simple test

--- __tests__/testMestrec.js
+++ __tests__/testMestrec.js
@@ -1,11 +1,41 @@
 'use strict';
 
-var Converter = require('../src/index');
-var fs = require('fs');
+const Converter = require('../src/index');
+const fs = require('fs');
+const route = __dirname + '/data/mestrec/';
 
 describe('Test from Mestrec Jcamp generator with assignment', function () {
-    it('should give 5 spectra', function () {
-        var result = Converter.createTree(fs.readFileSync(__dirname + '/data/mestrec/mestrec.jdx').toString());
+    it('real example', function () {
+        var result = Converter.createTree(fs.readFileSync(route + 'mestrec.jdx').toString());
         expect(result.length).toBe(1);
+    });
+
+    it('simple case', function () {
+        var result = Converter.createTree(fs.readFileSync(route + 'simple.jdx').toString());
+        expect(result).toEqual([{
+            title: 'first level 1',
+            dataType: 'first',
+            jcamp: '##TITLE= first level 1\n##DATATYPE=\tfirst\n',
+            children: [{
+                title: 'second level 1',
+                dataType: 'second',
+                jcamp: '##TITLE= second level 1\n##DATATYPE= second\n',
+                children: []
+            }, {
+                title: 'second level 2',
+                dataType: 'second',
+                jcamp: '##TITLE= second level 2\n##DATATYPE= second\n',
+                children: [{
+                    title: 'third level',
+                    jcamp: '##TITLE= third level\n',
+                    children: []
+                }]
+            }]
+        }, {
+            title: 'first level 2',
+            dataType: 'first',
+            jcamp: '##TITLE= first level 2\n##DATATYPE=\tfirst\n',
+            children: []
+        }]);
     });
 });
