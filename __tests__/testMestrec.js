66ae9f701a0e8a098327df997650a1e59c22cc94
Luc Patiny <luc@patiny.com>
2017-11-20 19:16:10 +0100

    add testMestrec

--- __tests__/testMestrec.js
+++ __tests__/testMestrec.js
@@ -5,16 +5,16 @@ var fs = require('fs');
 
 describe('Test from Mestrec Jcamp generator', function () {
 
-    it("NMR 1H spectrum", function () {
+    it('NMR 1H spectrum', function () {
 
-        var result = Converter.convert(fs.readFileSync(__dirname + "/data/mestrec/jcamp-256.jdx").toString(),
-            {xy:true}
+        var result = Converter.convert(fs.readFileSync(__dirname + '/data/mestrec/jcamp-256.jdx').toString(),
+            {xy: true}
         );
-        var data=result.spectra[0].data[0];
-        data.x.length.should.eql(256);
-        data.y.length.should.eql(256);
-       // console.log(data.x.length, Math.max(...data.x), Math.min(...data.x));
-       // console.log(data.y.length, Math.max(...data.y), Math.min(...data.y));
+        var data = result.spectra[0].data[0];
+        expect(data.x.length).toBe(256);
+        expect(data.y.length).toBe(256);
+        // console.log(data.x.length, Math.max(...data.x), Math.min(...data.x));
+        // console.log(data.y.length, Math.max(...data.y), Math.min(...data.y));
 
 
     });
