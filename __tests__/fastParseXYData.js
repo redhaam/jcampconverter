0bb09729d8afb620ecc00adf1e72f32c95345b8f
Michaël Zasso <targos@protonmail.com>
2019-06-13 14:51:48 +0200

    test: eslint fix

--- __tests__/fastParseXYData.js
+++ __tests__/fastParseXYData.js
@@ -7,17 +7,31 @@ const fs = require('fs');
 const options = {
   fastParse: true
 };
-var result = Converter.convert(fs.readFileSync(`${__dirname}/data/compression/jcamp-fix.dx`).toString(), options);
-var result2 = Converter.convert(fs.readFileSync(`${__dirname}/data/compression/jcamp-packed.dx`).toString(), options);
-var result3 = Converter.convert(fs.readFileSync(`${__dirname}/data/compression/jcamp-packed.dx`).toString(), options);
-var result4 = Converter.convert(fs.readFileSync(`${__dirname}/data/compression/jcamp-difdup.dx`).toString(), options);
-var result5 = Converter.convert(fs.readFileSync(`${__dirname}/data/compression/jcamp-difdup.dx`).toString());
+var result = Converter.convert(
+  fs.readFileSync(`${__dirname}/data/compression/jcamp-fix.dx`).toString(),
+  options
+);
+var result2 = Converter.convert(
+  fs.readFileSync(`${__dirname}/data/compression/jcamp-packed.dx`).toString(),
+  options
+);
+var result3 = Converter.convert(
+  fs.readFileSync(`${__dirname}/data/compression/jcamp-packed.dx`).toString(),
+  options
+);
+var result4 = Converter.convert(
+  fs.readFileSync(`${__dirname}/data/compression/jcamp-difdup.dx`).toString(),
+  options
+);
+var result5 = Converter.convert(
+  fs.readFileSync(`${__dirname}/data/compression/jcamp-difdup.dx`).toString()
+);
 
 describe('Test fastParseXYData', () => {
   it('It should yield exactly the same data', () => {
-    expect(result.spectra).toEqual(result2.spectra);
-    expect(result.spectra).toEqual(result3.spectra);
-    expect(result.spectra).toEqual(result4.spectra);
-    expect(result.spectra).toEqual(result5.spectra);
+    expect(result.spectra).toStrictEqual(result2.spectra);
+    expect(result.spectra).toStrictEqual(result3.spectra);
+    expect(result.spectra).toStrictEqual(result4.spectra);
+    expect(result.spectra).toStrictEqual(result5.spectra);
   });
 });
