b0d3187725aaf7efa5e0329f7713b0d77071d49b
Luc Patiny <luc@patiny.com>
2020-04-02 14:57:40 +0200

    chore: add some tests for trees

---
+++ debug/treeParsing.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const convert = require('../src').convert;
+
+var datafolder = join(__dirname, '../__tests__/data');
+
+var data = readFileSync(join(datafolder, 'tree-hsqc-1h13c.dx'), 'utf8');
+
+let result = convert(data, { noContour: true });
+
+console.log(result);
---
+++ debug/treeParsingUV.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const convert = require('../src').convert;
+
+var datafolder = join(__dirname, '../__tests__/data');
+
+var data = readFileSync(join(datafolder, 'tree-uv.jdx'), 'utf8');
+
+let result = convert(data);
+
+console.log(result);
