1c5237ecb7a01b442fd435f08e65eb3d37436dc3
Luc Patiny <luc@patiny.com>
2021-04-28 15:48:33 +0200

    chore: add ArrayBuffer parsing textcase

---
+++ __tests__/arrayBuffer.js
@@ -0,0 +1,22 @@
+import { readFileSync } from 'fs';
+
+import { convert } from '../src';
+
+test('from ArrayBuffer', () => {
+  const arrayBuffer = toArrayBuffer(readFileSync(`${__dirname}/data/jeol.dx`));
+  let result = convert(arrayBuffer);
+  let spectrum = result.flatten[0].spectra[0];
+  expect(arrayBuffer).toBeInstanceOf(ArrayBuffer);
+  expect(spectrum.firstX).toBeCloseTo(15.1029, 3);
+  expect(spectrum.lastX).toBeCloseTo(-4.88666, 4);
+  expect(spectrum.deltaX).toBeCloseTo(-0.0012201, 4);
+});
+
+function toArrayBuffer(buf) {
+  let ab = new ArrayBuffer(buf.length);
+  let view = new Uint8Array(ab);
+  for (let i = 0; i < buf.length; ++i) {
+    view[i] = buf[i];
+  }
+  return ab;
+}
