cbfb6037776c44fba01c151c003033749c370232
Luc Patiny <luc@patiny.com>
2021-11-05 07:38:39 +0100

    test: test the parsing of uint8array

--- __tests__/arrayBuffer.js
+++ __tests__/arrayBuffer.js
@@ -12,6 +12,15 @@ test('from ArrayBuffer', () => {
   expect(spectrum.deltaX).toBeCloseTo(-0.0012201, 4);
 });
 
+test('from Uint8Array', () => {
+  const blob = new Uint8Array(readFileSync(`${__dirname}/data/jeol.dx`));
+  let result = convert(blob);
+  let spectrum = result.flatten[0].spectra[0];
+  expect(spectrum.firstX).toBeCloseTo(15.1029, 3);
+  expect(spectrum.lastX).toBeCloseTo(-4.88666, 4);
+  expect(spectrum.deltaX).toBeCloseTo(-0.0012201, 4);
+});
+
 function toArrayBuffer(buf) {
   let ab = new ArrayBuffer(buf.length);
   let view = new Uint8Array(ab);
